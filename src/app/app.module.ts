65aeaa4e72b3474038f9d2454606e680a50bee92
Michael Moffet <mmoffet@michaels-mbp.home>
2017-12-16 18:59:49 -0800

    feat(app): initial commit
    Initial commit of changes to starter project for code challenge

---
+++ src/app/app.module.ts
@@ -0,0 +1,47 @@
+/// <reference path='../../declarations.d.ts'/>
+
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {RouterModule} from '@angular/router';
+
+import {AppComponent} from './app.component';
+import {NavigationComponent} from './Navigation';
+import AboutComponent from './about';
+import HomeComponent from './home';
+import {BlogComponent} from './blog/Blog';
+
+const routing = RouterModule.forRoot([
+    {
+        path: 'about',
+        component: AboutComponent
+    },
+    {
+      path: 'blog',
+      component: BlogComponent
+    },
+    {
+        path: 'home',
+        component: HomeComponent
+    }, {
+        path: '**',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+]);
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        routing,
+    ],
+    declarations: [
+        AppComponent,
+        NavigationComponent,
+        AboutComponent,
+        BlogComponent,
+        HomeComponent,
+    ],
+    bootstrap: [AppComponent]
+})
+export class AppModule {
+}
---
+++ src/app/blog/Blog.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+import template from './blog.html';
+
+@Component({
+    selector: 'blog',
+    template
+})
+
+export class BlogComponent {
+
+    constructor() {}
+}
