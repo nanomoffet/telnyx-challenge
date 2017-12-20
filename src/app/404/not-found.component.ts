acbb5da0b2de9bc2ec919c72c7561dd6f4b4bafe
Michael Moffet <mmoffet@michaels-mbp.home>
2017-12-19 18:14:17 -0800

    feat(app): Add 404 page for unmatched routes
    Add generic 404 component that gets routed to by the wildcard path in the router

---
+++ src/app/404/not-found.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+/**
+ * Basic template for 404 page. Currently only displays a simple message, eventually will contain additional functionality
+ *
+ */
+@Component({
+  template: '<h2>Page not found</h2>'
+})
+
+export class PageNotFoundComponent {}
