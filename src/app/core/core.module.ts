46de3598f8d727bc2ff1fa6ce46df14fe3a64405
Michael Moffet <mmoffet@michaels-mbp.home>
2017-12-21 09:25:24 -0800

    chore(core): Add safety check to core module
    Add check to prevent core module from being imported into the app more than once.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,4 +1,4 @@
-import {NgModule} from '@angular/core';
+import {NgModule, Optional, SkipSelf} from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {BrowserModule} from '@angular/platform-browser';
 import {PostsService} from '../blog/posts.service';
@@ -21,4 +21,11 @@ import {HttpClientModule} from '@angular/common/http';
     ]
 })
 export class CoreModule {
+    constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
+        if (parentModule) {
+            throw new Error(
+                'CoreModule is already loaded. Import it in the AppModule only'
+            );
+        }
+    }
 }
