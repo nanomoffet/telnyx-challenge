f38d995bd202551bdde84ff507ad490c7d026f8d
Michael Moffet <mmoffet@michaels-mbp.home>
2017-12-21 09:25:58 -0800

    feat(blog): WIP update to blog
    Fix ngFor loop for posts

--- src/app/blog/blog.module.ts
+++ src/app/blog/blog.module.ts
@@ -2,9 +2,11 @@ import {NgModule} from '@angular/core';
 import {BlogComponent} from './Blog';
 import {PostSummaryComponent} from './post-summary/post-summary';
 import {PostDetailComponent} from './post-summary/post-detail/post-detail';
+import {SharedModule} from '../shared/shared.module';
 
 @NgModule({
     imports: [
+        SharedModule
     ],
     declarations: [
         BlogComponent,
